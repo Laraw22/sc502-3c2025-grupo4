5d56877db7abdefde10c7feafbeae2cc3ec05e2f
larawil <larawilfredo24@gmail.com>
2025-03-22 16:27:19 -0600

    colocación de todos los archivos css y las vistas

---
+++ sc502-3c2025-grupo4/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Voluntariado</title>
+    <meta charset="UTF-8" />
+    <link
+      rel="stylesheet"
+      href="../sc502-3c2025-grupo4/public/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../sc502-3c2025-grupo4/public/css/estilos.css"
+    />
+    <link rel="stylesheet" href="../sc502-3c2025-grupo4/public/css/menu.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    <script src="../views/ArchJavaSc/menuDes.js"></script>
+  </head>
+
+  <body>
+    <header class="header">
+      <div class="espacio_titulo">
+        <a href="plantilla.html" class="link log">Voluntariado</a>
+      </div>
+      <br />
+      <ul class="menu">
+        <li class="menu-item">
+          <a href="" class="link menu-link">Nosotros</a>
+        </li>
+        <li class="menu-item">
+          <a href="Noticias/noticias.html" class="link menu-link">Noticias</a>
+        </li>
+        <li class="menu-item">
+          <a href="Buscar/Buscar.html" class="link menu-link">Buscar</a>
+        </li>
+        <div class="icon-esquin">
+          <li class="menu-item">
+            <a href="inicio sesion/inicioSesion.html">
+              <i class="fa-solid fa-user"></i
+            ></a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="menu-icon" id="menuToggle">
+              <i class="fa-solid fa-bars"></i>
+            </a>
+          </li>
+        </div>
+      </ul>
+      <br />
+    </header>
+    <div class="side-menu" id="sideMenu">
+      <ul>
+        <li><a href="perfil/perfil.html">Perfil</a></li>
+        <li><a href="#">.....</a></li>
+        <li><a href="#">.....</a></li>
+      </ul>
+    </div>
+    <aside>
+      <br />
+    </aside>
+    <div>
+      <h1>Nosotros</h1>
+      <img
+        src="https://th.bing.com/th/id/R.35bfd92ac2d4c44394702f0c62aec3d3?rik=u81EtLB%2fotuC7A&riu=http%3a%2f%2fwww.ushispanicministry.com%2fwp-content%2fuploads%2f2015%2f01%2fvoluntarios.jpg&ehk=yTOEJb5oYodZ2Jb%2bm%2bUy%2fCN32cb4wNC4hqlMFVR1LmQ%3d&risl=&pid=ImgRaw&r=0"
+        class="img-fluid"
+        alt=""
+        style="width: 100%; height: 500px; object-fit: cover"
+      />
+    </div>
+    <div class="contenedor-quienes-somos">
+      <div class="container">
+        <h2>¿Quiénes somos?</h2>
+        <p>
+          Somos un grupo de estudiantes universitarios comprometidos con
+          impulsar el voluntariado a nivel nacional. Al analizar la realidad
+          actual, identificamos un problema clave: la falta de una plataforma
+          accesible y centralizada para conectar a voluntarios con
+          organizaciones que necesitan apoyo.
+        </p>
+        <p>
+          El voluntariado es una herramienta poderosa para fortalecer la
+          sociedad y contribuir en áreas como la educación, el medioambiente, la
+          salud y la asistencia social. Sin embargo, muchas iniciativas carecen
+          de visibilidad y medios eficientes para reclutar voluntarios.
+        </p>
+        <p>
+          Por ello, desarrollamos una solución digital que facilita la conexión
+          entre organizaciones y personas interesadas en contribuir. Nuestro
+          objetivo es crear un espacio donde cualquier persona pueda encontrar
+          oportunidades de voluntariado de manera sencilla y participar
+          activamente en causas que generen impacto positivo.
+        </p>
+      </div>
+      <div class="imagen">
+        <img src="images/voluntariado una mano amiga.PNG" alt="Voluntariado" />
+      </div>
+    </div>
+    <div class="contenedor-quienes-somos">
+      <div class="container">
+        <h2>Algunos tipos de voluntariado</h2>
+        <p>
+          Te ofrecemos una amplia gama de opciones, desde voluntariado social y
+          ambiental hasta proyectos educativos y culturales, y mucho más con
+          nuestra herramienta de búsqueda fácil de usar, podrás encontrar la
+          oportunidad perfecta para ti, ya sea para crear tu propio proyecto o
+          unirte a una causa que te apasione.
+        </p>
+        <ul>
+          <li><strong>Voluntariado Social</strong></li>
+          <li><strong>Voluntariado Ambiental</strong></li>
+          <li><strong>Voluntariado Educativo</strong></li>
+          <li><strong>Voluntariado Animales</strong></li>
+        </ul>
+      </div>
+    </div>
+    <div class="contenedor-izquierda">
+      <div class="container-izquierda">
+        <h2>¿Que es el voluntariado social?</h2>
+        <p>
+          El voluntariado social es una actividad que no tienes fines de lucro
+          ya que se hace por una interesante cantidad de persona que por ellos
+          mismo tiene iniciativa de tener empatia con la vida de otras personas
+          con el fin de ayuadarlas con todo lo que puedan para darles una mejor
+          calidad de vida.
+        </p>
+      </div>
+      <div class="imagen">
+        <img src="images/social.jpeg" alt="Voluntariado" />
+      </div>
+    </div>
+    <div class="contenedor-quienes-somos">
+      <div class="container">
+        <h2>¿Que es el voluntariado ambiental?</h2>
+        <p>
+          El voluntariado ambiental como un conjunto de acciones realizadas de
+          manera voluntaria, cuyo fin es preservar y restaurar el medioambiente.
+          Estas actividades abarcan desde la restauración de ecosistemas
+          deteriorados hasta la reducción de residuos, pasando por la promoción
+          de la educación ambiental, con miras a sensibilizar a la población
+          sobre la importancia de cuidar el planeta.
+        </p>
+      </div>
+      <div class="imagen">
+        <img src="images/ANBIENTE.jpg" alt="Voluntariado" />
+      </div>
+    </div>
+    <div class="contenedor-izquierda">
+      <div class="container-izquierda">
+        <h2>¿Que es el voluntariado Educativo?</h2>
+        <p>
+          El voluntariado educativo es una actividad fundamental para la
+          contruccion de una sociedad mas justa y equitativa este tipo de
+          voluntariado no solo se usa para el bienestar de la comunidad tambien
+          fortalece los tejido sociales y promueve la solidaridad.
+        </p>
+      </div>
+      <div class="imagen">
+        <img src="images/1.png" alt="Voluntariado" />
+      </div>
+    </div>
+    <div class="contenedor-quienes-somos">
+      <div class="container">
+        <h2>¿Que es el voluntariado Animales?</h2>
+        <p>
+          El voluntariado ambiental se un conjunto de acciones realizadas de
+          manera voluntaria, cuyo fin es preservar y restaurar el medioambiente.
+          Estas actividades abarcan desde la restauración de ecosistemas
+          deteriorados hasta la reducción de residuos, pasando por la promoción
+          de la educación ambiental, con miras a sensibilizar a la población
+          sobre la importancia de cuidar el planeta.
+        </p>
+      </div>
+      <div class="imagen">
+        <img src="images/_Turtle_.webp" alt="Voluntariado" />
+      </div>
+    </div>
+    <footer class="foot">
+      <br />
+      <p>Maneras de contacto</p>
+      <div class="sociales" aria-hidden="true">
+        <a href="https://www.instagram.com/"
+          ><i class="fa-brands fa-instagram"></i
+        ></a>
+        <a href="https://www.whatsapp.com/?lang=es_LA"
+          ><i class="fa-brands fa-whatsapp"></i
+        ></a>
+        <a href="https://www.facebook.com/?locale=es_LA"
+          ><i class="fa-brands fa-facebook"></i
+        ></a>
+      </div>
+      <div></div>
+    </footer>
+  </body>
+</html>
