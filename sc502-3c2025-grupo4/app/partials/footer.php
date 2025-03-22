2978a19f18741f1c2650cff25c088457b68e2f4f
larawil <larawilfredo24@gmail.com>
2025-03-22 17:14:47 -0600

    Agregado las opciones de include del menu, footer

---
+++ sc502-3c2025-grupo4/app/partials/footer.php
@@ -0,0 +1,19 @@
+
+    <footer class="foot">
+      <br />
+      <p>Maneras de contacto</p>
+      <div class="sociales" aria-hidden="true">
+        <a href="https://www.instagram.com/"
+          ><i class="fa-brands fa-instagram"></i
+        ></a>
+        <a href="https://www.whatsapp.com/?lang=es_LA"
+          ><i class="fa-brands fa-whatsapp"></i
+        ></a>
+        <a href="https://www.facebook.com/?locale=es_LA"
+          ><i class="fa-brands fa-facebook"></i
+        ></a>
+      </div>
+      <div></div>
+    </footer>
+  </body>
+</html>
---
+++ sc502-3c2025-grupo4/app/partials/menu.php
@@ -0,0 +1,63 @@
+
+
+<html>
+  <head>
+    <title>Voluntariado</title>
+    <meta charset="UTF-8" />
+    <link
+      rel="stylesheet"
+      href="../sc502-3c2025-grupo4/public/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="../sc502-3c2025-grupo4/public/css/estilos.css"
+    />
+    <link rel="stylesheet" href="../sc502-3c2025-grupo4/public/css/menu.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    <script src="../views/ArchJavaSc/menuDes.js"></script>
+  </head>
+
+  <body>
+    <header class="header">
+      <div class="espacio_titulo">
+        <a href="plantilla.html" class="link log">Voluntariado</a>
+      </div>
+      <br />
+      <ul class="menu">
+        <li class="menu-item">
+          <a href="" class="link menu-link">Nosotros</a>
+        </li>
+        <li class="menu-item">
+          <a href="Noticias/noticias.html" class="link menu-link">Noticias</a>
+        </li>
+        <li class="menu-item">
+          <a href="Buscar/Buscar.html" class="link menu-link">Buscar</a>
+        </li>
+        <div class="icon-esquin">
+          <li class="menu-item">
+            <a href="inicio sesion/inicioSesion.html">
+              <i class="fa-solid fa-user"></i
+            ></a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="menu-icon" id="menuToggle">
+              <i class="fa-solid fa-bars"></i>
+            </a>
+          </li>
+        </div>
+      </ul>
+      <br />
+    </header>
+    <div class="side-menu" id="sideMenu">
+      <ul>
+        <li><a href="perfil/perfil.html">Perfil</a></li>
+        <li><a href="#">.....</a></li>
+        <li><a href="#">.....</a></li>
+      </ul>
+    </div>
+    <aside>
+      <br />
+    </aside>
