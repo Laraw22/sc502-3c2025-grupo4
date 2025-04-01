995690caefefdaa8dfb98da7d74ab1d9bb7974dc
Caleb-Sacasa <caleborus24@gmail.com>
2025-04-01 16:40:57 -0600

    Lo mejore lo de crear voluntariado

--- sc502-3c2025-grupo4/app/views/Voluntariado/CrearVoluntariado.php
+++ sc502-3c2025-grupo4/app/views/Voluntariado/CrearVoluntariado.php
@@ -1,45 +1,50 @@
 <h1>Crear Voluntariado</h1>
 <form class="row g-3">
   <div class="col-md-6">
-    <label for="inputNombre" class="form-label">Nombre</label>
-    <input type="Nombre" class="form-control" id="inputNombre">
+    <label for="inputNombre" class="form-label">Nombre del Voluntariado</label>
+    <input type="text" class="form-control" id="inputNombre" placeholder="Ejemplo: Ayuda Comunitaria">
   </div>
   <div class="col-md-6">
-    <label for="inputdescripcion" class="form-label">Descripcion</label>
-    <input type="descripcion" class="form-control" id="inputdescripcion">
+    <label for="inputDescripcion" class="form-label">Descripción</label>
+    <textarea class="form-control" id="inputDescripcion" rows="3" placeholder="Breve descripción del voluntariado"></textarea>
   </div>
   <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+    <label for="inputDireccion" class="form-label">Dirección</label>
+    <input type="text" class="form-control" id="inputDireccion" placeholder="Ejemplo: Calle 123, Ciudad">
   </div>
   <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity">
+    <label for="inputCiudad" class="form-label">Ciudad</label>
+    <input type="text" class="form-control" id="inputCiudad" placeholder="Ejemplo: Tibas">
   </div>
   <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
+    <label for="inputEstado" class="form-label">Estado/Provincia</label>
+    <select id="inputEstado" class="form-select">
+      <option selected>Seleccione...</option>
+      <option>San jose</option>
+      <option>Limon</option>
+      <option>Puntarenas</option>
+      <option>Cartago</option>
+      <option>Heredia</option>
+      <option>Guanacaste</option>
+      <option>Alajuela</option> 
     </select>
   </div>
   <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip">
-  </div>
+    <label for="inputCodigoPostal" class="form-label">Código Postal</label>
+    <input type="text" class="form-control" id="inputCodigoPostal" placeholder="Ejemplo: 70101">
+  </div><div class="mb-3">
+  <label for="formFile" class="form-label">Agregar imagen</label>
+  <input class="form-control" type="file" id="formFile">
+</div>
   <div class="col-12">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck">
       <label class="form-check-label" for="gridCheck">
-        Check me out
+        Acepto los términos y condiciones
       </label>
     </div>
   </div>
   <div class="col-12">
-    <button type="submit" class="btn btn-primary">Agregar</button>
+    <button type="submit" class="btn btn-primary">Agregar Voluntariado</button>
   </div>
 </form>
