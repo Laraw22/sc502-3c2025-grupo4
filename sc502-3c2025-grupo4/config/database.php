bdecbefd182efc99aad2c63652a04fd0d2ce8625
Caleb-Sacasa <caleborus24@gmail.com>
2025-04-11 22:30:09 -0600

    correcion

--- sc502-3c2025-grupo4/config/database.php
+++ sc502-3c2025-grupo4/config/database.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "voluntariados";
+$database = "voluntariado";
 
 try {
     $conn = new mysqli($host, $user, $password, $database);
